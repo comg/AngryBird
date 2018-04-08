f8b7d69100132f8275fa40ea27286f704e84b18b
comg <1527455626@qq.com>
2018-04-08 17:50:03 +0800

    公司电脑提交

---
+++ Assets/scripts/lose.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lose : MonoBehaviour {
+
+	private void hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
